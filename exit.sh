3aa4e063ae272caa2942d81e32de5003355a3870
Ahmed_Dadousha <bioshock44matrix44@gmail.com>
2024-03-09 18:22:42 +0200

    Feat: Scripts folder

--- exit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-choice=$(echo -e "Shutdown\nReboot\nExit" | dmenu -i -p "choose action: " )
-
-[ $choice = "Shutdown" ] && doas poweroff
-[ $choice = "Reboot" ] && doas reboot
-[ $choice = "Exit" ] && pkill dwm
--- volume.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-arg=$1
-[ $arg = "up" ] && amixer sset Master 5%+
-[ $arg = "down" ] && amixer sset Master 5%-
-[ $arg = "toggle" ] && amixer sset Master toggle
