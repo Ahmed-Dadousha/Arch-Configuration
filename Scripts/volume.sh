fb49c9124e020ccda2905dcc52c08e04bc4962d3
Ahmed_Dadousha <103436974+Ahmed-Dadousha@users.noreply.github.com>
2024-03-14 00:21:48 +0200

    Update volume.sh

--- Scripts/volume.sh
+++ Scripts/volume.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 arg=$1
-[ $arg = "up" ] && amixer sset Master 5%+
-[ $arg = "down" ] && amixer sset Master 5%-
+[ $arg = "up" ] && amixer sset Master 1%+
+[ $arg = "down" ] && amixer sset Master 1%-
 [ $arg = "toggle" ] && amixer sset Master toggle
