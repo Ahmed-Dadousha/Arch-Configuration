a5a5f0a758fa3ec98c4eb725c1df63ac02b69a34
Ahmed_Dadousha <bioshock44matrix44@gmail.com>
2024-03-19 14:40:08 +0200

    Feat: Battery script

--- Suckless/dwmblocks/blocks.def.h
+++ Suckless/dwmblocks/blocks.def.h
@@ -2,9 +2,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"VOL:", "amixer get Master | grep -q '\\[off\\]' && echo 'NON' || amixer get Master | awk -F'[][]' 'END{ print $2 }' ", 1, 10},
-	{"RAM:", "~/memory.sh",							  1, 0},
+	{"RAM:", "memory.sh",							  1, 0},
 	{"DATE:","date '+%a %d %b | Time: %I:%M:%S %p'" , 									  1, 0},
-	{"LANG: ", "setxkbmap -query | awk '/layout/{print ($2==\"us\")?\"eng\":$2}'", 						  1, 10}
+	{"LANG:", "setxkbmap -query | awk '/layout/{print ($2==\"us\")?\"eng\":$2}'", 						  1, 10},
+	{"BATT:", "battery.sh",                                                                                                   1, 0}
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
