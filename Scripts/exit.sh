44555ad30093496defd41d17dff771daa986e9a2
Ahmed_Dadousha <bioshock44matrix44@gmail.com>
2024-03-19 02:53:22 +0200

    Feat: Added font and colors to exit script

--- Scripts/exit.sh
+++ Scripts/exit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-choice=$(echo -e "Shutdown\nReboot\nExit" | dmenu -l 3 -p "choose action: " )
+choice=$(echo -e "Shutdown\nReboot\nExit" | dmenu  -fn "MesloLGS NF:size=12"  -nf "#bbbbbb" -sf "#bbbbbb" -l 3 -p "choose action: " )
 
 [ $choice = "Shutdown" ] && doas poweroff
 [ $choice = "Reboot" ] && doas reboot
